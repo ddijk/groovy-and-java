fa60d4f96fb0b6d5685bdba605038a877a770d2a
ddijk <dick@dijkrosoft.nl>
2021-10-18 20:47:18 +0200

    Working: compile all Java and Groovy code and run all tests

--- src/test/groovy/nl/dijkrosoft/MyTest.groovy
+++ src/test/groovy/nl/dijkrosoft/MyTest.groovy
@@ -1,38 +1,37 @@
 package nl.dijkrosoft
 
-import groovy.test.GroovyTestCase
-import org.junit.Ignore
+
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestFactory
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 import java.util.stream.Stream
 
-//@Ignore
-class MyTest extends GroovyTestCase {
+class MyTest {
     @Test
     void streamSum() {
-        assertTrue(Stream.of(1, 2, 3).mapToInt(i -> i).sum() > 5, () -> "Sum should be greater than 5")
+        Assertions.assertTrue(Stream.of(1, 2, 3).mapToInt(i -> i).sum() > 5, () -> "Sum should be greater than 5")
     }
 
-    @RepeatedTest(value=2, name = "{displayName} {currentRepetition}/{totalRepetitions}")
+    @RepeatedTest(value = 2, name = "{displayName} {currentRepetition}/{totalRepetitions}")
     void streamSumRepeated() {
         assert Stream.of(1, 2, 3).mapToInt(i -> i).sum() == 6
     }
 
-    private boolean isPalindrome(s) { s == s.reverse()  }
+    private boolean isPalindrome(s) { s == s.reverse() }
 
     @ParameterizedTest
-    @ValueSource(strings = [ "racecar", "radar", "able was I ere I saw elba" ])
+    @ValueSource(strings = ["racecar", "radar", "able was I ere I saw elba", "legovogel"])
     void palindromes(String candidate) {
         assert isPalindrome(candidate)
     }
 
-//    @TestFactory
-//    def dynamicTestCollection() {[
-//            dynamicTest("Add test") { -> assert 1 + 1 == 2 },
-//            dynamicTest("Multiply Test", () -> { assert 2 * 3 == 6 })
-//    ]}
+    @Test
+    void testMe() {
+        def arr = [[1, 2, 3], [10, 11, 12]]
+
+        assert [[1, 10], [1, 11], [1, 12], [3, 10], [3, 11], [3, 12], [2, 10], [2, 11], [2, 12]] as Set == arr.combinations() as Set
+    }
 }
